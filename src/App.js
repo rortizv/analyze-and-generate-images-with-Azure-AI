1ad0960f69cf1c046f46f1c2630690e80e9968cb
Rafael Ortiz <rortiz@inkremental.co>
2024-02-19 17:31:39 -0500

    Add loader and style

--- src/App.js
+++ src/App.js
@@ -1,27 +1,73 @@
 import React, { useState } from 'react';
+import './index.css';
 
 function App() {
   const [imageUrl, setImageUrl] = useState('');
+  const [analysisResult, setAnalysisResult] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleAnalyzeClick = () => {
-    // TODO: Implement image analysis functionality
+  const key = 'YOUR_KEY_HERE';
+  const endpoint = 'https://YOUR';
+
+  const analyzeImage = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${endpoint}/computervision/imageanalysis:analyze?api-version=2023-10-01&features=tags,read,caption,denseCaptions,smartCrops,objects,people`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Ocp-Apim-Subscription-Key': key,
+        },
+        body: JSON.stringify({
+          url: imageUrl,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Image analysis failed');
+      }
+
+      const result = await response.json();
+      setAnalysisResult(result);
+    } catch (error) {
+      console.error('Error analyzing image:', error);
+      setAnalysisResult(null);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  const handleGenerateClick = () => {
-    // TODO: Implement image generation functionality
+  const clearResponse = () => {
+    setAnalysisResult(null);
   };
+
+  const clearInput = () => {
+    setImageUrl('');
+  }
 
   return (
-    <div>
-      <h1>Image Analyzer</h1>
-      <input
-        type="text"
-        value={imageUrl}
-        onChange={(e) => setImageUrl(e.target.value)}
-        placeholder="Enter image URL"
-      />
-      <button onClick={handleAnalyzeClick}>Analyze</button>
-      <button onClick={handleGenerateClick}>Generate</button>
+    <div className='main'>
+      <h1>Azure Image Analyzer</h1>
+        <input
+          type="text"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+          placeholder="Enter image URL"
+        />
+      <div className="buttons-container">
+        <button id='analyze' onClick={analyzeImage} disabled={isLoading || imageUrl === ''}>Analyze</button>
+        <button id='clear-input' onClick={clearInput}>Clear Input</button>
+        <button id='clear-response' onClick={clearResponse} disabled={isLoading}>Clear Response</button>
+      </div>
+
+      {isLoading && <div className='loader'></div>}
+
+      {analysisResult && (
+        <div className='result'>
+          <h2>Analysis Result:</h2>
+          <pre>{JSON.stringify(analysisResult, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 }
